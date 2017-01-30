de3af46155f19673288ea6fae385133de23dd214
Kabage <kabageedward@gmail.com>
2017-01-30 20:28:42 +0300

    save teacher data from user

--- start.py
+++ start.py
@@ -1,4 +1,6 @@
 import peewee
+from teachers import *
+import sys
 
 def main():
     print "K-12\n"
@@ -22,19 +24,45 @@ def main():
             print "create student record"
         elif selected_operation=="2":
             print "create teacher record"
+            create_teacher_record("dfgrgf")
         else:
             print "Operation not supported"
 
 def create_student_record(school_name):
+    
     pass
 
 def create_teacher_record(school_name):
+
+    first_name = raw_input("Enter first name: \n")
+    if input_valid(first_name):
+        second_name= raw_input("Enter second Name: \n")
+        if input_valid(second_name):
+            grade_level=raw_input("Enter grade level: \n")
+            if input_valid(grade_level):
+
+                teacher_details=TeacherDetails()
+                teacher_details.first_name=first_name
+                teacher_details.second_name=second_name
+                teacher_details.grade_level=grade_level
+                teacher_details.save_details()
+
+            else:
+                print "Input invalid"
+        else:
+            print "Input invalid"
+    else:
+        print "Input invalid"
+
     
-    teacher_details=TeacherDetails()
-    teacher_details.first_name="edward"
-    teacher_details.second_name="mwangi"
-    teacher_details.grade_level="g1"
-    teacher_details.save_details()
+
+def input_valid(value):
+    if len(value) ==0 or value==None:
+        print "Value is invalid"
+        return False
+    else:
+        return True
+        
 
 
 if __name__=='__main__':
